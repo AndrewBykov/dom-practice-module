b8a70d08bbe0923c479889b633e74c24e92c788a
Andrew Kirilkin <124495133+AndrewBykov@users.noreply.github.com>
2023-11-21 15:15:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const inputButton = document.querySelector('#btn')
+const inputAnswear = document.querySelector('#result')
+
+inputButton.addEventListener('click', answearText)
+
+
+function calculation(a, b, c, action) {
+
+  if (a === '' || b === '' || c === '' || action === '') {
+    return 'Введите все поля ввода.'
+  }
+  
+  const num1 = +a
+  const num2 = +b
+  const numModule = +c
+
+  let sum =  `${num1} ${action} ${num2}`;
+  let sum1 = eval(sum)
+
+  if (action === '+' || action === '-' || action === '*' || action === '/') {
+    if (sum1 > numModule) {
+      return `Ваш ответ: ${sum} = ${sum1 - numModule} (модуль: ${numModule})`
+    } else {
+      return `Ваш ответ: ${sum} = ${sum1} (модуль: ${numModule})`
+    }
+  } else {
+    return 'Доступно всего лишь 4 действия (+, -, *, /).'
+  }
+
+}
+
+function answearText() {
+  const inputA = document.querySelector('#input-a').value
+  const inputB = document.querySelector('#input-b').value
+  const inputC = document.querySelector('#input-c').value
+  const inputActions = document.querySelector('#input-actions').value
+
+  const variableFun = calculation(inputA, inputB, inputC, inputActions)
+  inputAnswear.innerHTML = variableFun
+}
