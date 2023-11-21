48def6b6403946a5526f3a411a4a7fa57c2e511a
Andrew Kirilkin <124495133+AndrewBykov@users.noreply.github.com>
2023-11-21 15:48:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,41 @@
-const inputButton = document.querySelector('#btn')
-const inputAnswear = document.querySelector('#result')
+const inputButton = document.querySelector('#btn');
+const inputAnswear = document.querySelector('#result');
 
-inputButton.addEventListener('click', answearText)
-
+inputButton.addEventListener('click', answerText);
 
 function calculation(a, b, c, action) {
-
   if (a === '' || b === '' || c === '' || action === '') {
-    return 'Введите все поля ввода.'
+    return 'Введите все поля ввода.';
   }
-  
-  const num1 = +a
-  const num2 = +b
-  const numModule = +c
 
-  let sum =  `${num1} ${action} ${num2}`;
-  let sum1 = eval(sum)
+  const num1 = +a;
+  const num2 = +b;
+  const numModule = +c;
 
-  if (action === '+' || action === '-' || action === '*' || action === '/') {
+  let sum = `${num1} ${action} ${num2}`;
+  let sum1 = eval(sum);
+
+  if (action === '+' || action === '*' || action === '**') {
+    while (sum1 > numModule) {
+      sum1 = sum1 / 2;
+    }
+
     if (sum1 > numModule) {
-      return `Ваш ответ: ${sum} = ${sum1 - numModule} (модуль: ${numModule})`
+      return `Ваш ответ: ${sum} = ${Math.round(sum1)} (модуль: ${numModule})`;
     } else {
-      return `Ваш ответ: ${sum} = ${sum1} (модуль: ${numModule})`
+      return `Ваш ответ: ${sum} = ${Math.round(sum1)} (модуль: ${numModule})`;
     }
   } else {
-    return 'Доступно всего лишь 4 действия (+, -, *, /).'
+    return 'Доступно всего лишь 4 действия (+, -, *, /).';
   }
-
 }
 
-function answearText() {
-  const inputA = document.querySelector('#input-a').value
-  const inputB = document.querySelector('#input-b').value
-  const inputC = document.querySelector('#input-c').value
-  const inputActions = document.querySelector('#input-actions').value
+function answerText() {
+  const inputA = document.querySelector('#input-a').value;
+  const inputB = document.querySelector('#input-b').value;
+  const inputC = document.querySelector('#input-c').value;
+  const inputActions = document.querySelector('#input-actions').value;
 
-  const variableFun = calculation(inputA, inputB, inputC, inputActions)
-  inputAnswear.innerHTML = variableFun
+  const variableFun = calculation(inputA, inputB, inputC, inputActions);
+  inputAnswear.innerHTML = variableFun;
 }
